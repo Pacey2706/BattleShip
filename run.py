fe30b9235e65157fa70b96037a6306cca608c69b
Pacey2706 <pacey2706@gmail.com>
2023-03-12 22:15:50 +0000

    add game logo

--- run.py
+++ run.py
@@ -1,10 +1,33 @@
+print(r"""   
+ _           _   _   _           _     _  
+| |         | | | | | |         | |   (_)      
+| |__   __ _| |_| |_| | ___  ___| |__  _ _ __  
+| '_ \ / _` | __| __| |/ _ \/ __| '_ \| | '_ \ 
+| |_) | (_| | |_| |_| |  __/\__ \ | | | | |_) |
+|_.__/ \__,_|\__|\__|_|\___||___/_| |_|_| .__/ 
+                                        | |    
+                                        |_|       
+             )_)  )_)  )_)
+            )___))___))___)\
+           )____)____)_____)\\
+         _____|____|____|____\\\__
+---------\                   /---------""")
+
 PLAYER_BOARD = []#set boards as 5/5
 COMPUTER_BOARD =[ ]
 
-def print_board:
+def print_board():
     pass
 
-def create_computerships:
+def create_computerships():
     pass
 
-def 
+def computers_turn():
+    pass
+
+def players_turn():
+    pass
+
+def main():
+    pass
+
